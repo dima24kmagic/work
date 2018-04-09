743b947f4f6edc0fe7900b4754013379be0ba58c
Dima Baranov <vajnoe-dima@mail.ru>
2018-04-09 16:54:49 +0300

    plug redux dev toold start creating form

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,8 +3,19 @@ import React, {Component} from 'react';
 class Users extends Component{
   render(){
     return(
-      <div className="home">
-        <h2 className="home__intro">That's Users content</h2>
+      <div className="users">
+        <div className="users__form">
+          <form className="form">
+            <div className="group">
+              <input className="group__item" type="text" id="fname" placeholder="First Name"/>
+              <label className="group__item__label" htmlFor="fname">First Name</label>
+            </div>
+            <div className="group">
+              <input className="group__item" type="text" id="lname" placeholder="Last Name"/>
+              <label className="group__item__label" htmlFor="lname">Last Name</label>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
