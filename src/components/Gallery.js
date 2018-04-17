32ba43181a91c926ab8730627e63e32fb44376d5
Dima Baranov <vajnoe-dima@mail.ru>
2018-04-17 17:09:37 +0300

    one bug left + button don't work

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -28,7 +28,7 @@ class Gallery extends Component{
     super(props);
     this.state = {
       loading: false,
-      images: {}
+      imgLoaded: 1
     }
   }
   startSearch = (e) =>{
@@ -68,13 +68,33 @@ class Gallery extends Component{
           }
           {
             this.props.getStoreState('images').map((image, i) => {
-              // console.log(111);
+              /*
+              So, when onSearch Function start, it's set to store urls of images
+              When it's does, our store subscribe is triggered
+              and this component start to rendering
+              --- Preloader shown when we start typing,
+              and it's gonna hide, when at all of the images triggered
+              onLoad event and they increment counter of loaded images
+              if length of all images loaded equal to length of images from the store
+              we gonna set loading to false
+              */
+
+              /* ---BUG: Whn we press space and then backspace - preloader is shown
+              but it's never goes back, because store value don't change
+              and images don't rendering. so, onLoad event doesnt executed
+
+              ---UPD: Gonna try to trouble shoot this with
+              if/else statement on fetching data, if data that''s fetched EQUAL to data
+              that's already in store - don't do the dispatch
+              */
               return(
                 <div className="col-12 col-sm-6 col-lg-4 gal__container" key={i}>
                   <img className={"d-"+this.state.imgDisplay+" gal__pic"} src={image.url} alt={image.title}
                     onLoad={()=>{
-                      if(i+1 == this.props.getStoreState('images').length){
-                        this.setState({loading: false})
+                      if(this.state.imgLoaded == this.props.getStoreState('images').length){
+                        this.setState({loading: false, imgLoaded: 1})
+                      }else{
+                        this.setState({imgLoaded: this.state.imgLoaded+1})
                       }
                     }}></img>
                 </div>
